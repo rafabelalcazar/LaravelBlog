15379993b939b451191e3881e439cfac5013ce51
rafabelalcazar <rafaelb@unicauca.edu.co>
2020-11-01 22:46:05 -0500

    FEAT: Validating data in POST method:UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 use App\Models\Post;
 use App\Models\Category;
 
@@ -15,31 +16,63 @@ class UserController extends Controller
     public function register(Request $request)
     {
         // Catch user data from POST method
-
         $body = $request->only([
             'name',
             'surname',
             'description',
             'email',
             'password',
+        ]); //return an array
+
+
+        // Validating data
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|unique:users|max:50',
+            'surname' => 'required',
+            'description' => 'required',
+            'email' => 'required',
+            'password' => 'required',
         ]);
 
+        if ($validator->fails()) { 
+            return response()->json($validator->errors(),400);
+        }
+        return response()->json($body);
+
+        // $validateData = $request->validate([
+        //     'name'=>['required']
+        // ]);
+
+        
+        // if($validateData){
+        //     return response()->json($validateData);
+        // }
+        // else{
+
+        // }
+        
+
+        // $json = json_decode($body);
+
+        // var_dump($json);
+        // die();
         // return "${body}";
 
         // Validate data
+
         // Valedate if user exists
         // crypt password
         // Create user
 
-        $data = [
-            'status' =>'error',
-            'code'=>404,
-            'message'=>'El usuario no se ha creado',
-            'data'=>$body
-        ];
+        // $data = [
+        //     'status' =>'error',
+        //     'code'=>404,
+        //     'message'=>'El usuario no se ha creado',
+        //     'data'=>$body
+        // ];
 
 
-        return response()->json($data,$data['code']);
+        // return response()->json($data,$data['code']);
     }
 
     public function login(Request $request)
